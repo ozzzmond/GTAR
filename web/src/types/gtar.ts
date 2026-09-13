4d4f51d31a8fbb79dea85126b8a80116c24c033a
ozzzmond <jlopez3rd@gmail.com>
2026-09-13 10:09:48 +0800

    chore(web): start web v1.0.86-dev.1

--- web/src/types/gtar.ts
+++ web/src/types/gtar.ts
@@ -3,7 +3,7 @@
  */
 
 export const GTAR_APP_VERSION = '1.1.86';
-export const GTAR_DEV_VERSION = '1.0.85-dev.1';
+export const GTAR_DEV_VERSION = '1.0.86-dev.1';
 export const GTAR_SETLIST_VERSION = 1
 export const GTAR_SETLIST_TYPE = 'GTAR_SETLIST'
 
